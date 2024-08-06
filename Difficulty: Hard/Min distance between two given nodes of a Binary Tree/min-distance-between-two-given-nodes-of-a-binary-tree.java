c0427eeb907e6b1b880c16008c0386031b5bf005
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2024-08-06 21:13:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Min distance between two given nodes of a Binary Tree/min-distance-between-two-given-nodes-of-a-binary-tree.java
@@ -0,0 +1,149 @@
+//{ Driver Code Starts
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Main {
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            String s = br.readLine().trim();
+            Node root = buildTree(s);
+            String[] ab = br.readLine().trim().split(" ");
+            int a = Integer.parseInt(ab[0]);
+            int b = Integer.parseInt(ab[1]);
+
+            GfG g = new GfG();
+            System.out.println(g.findDist(root, a, b));
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+
+
+// FUNCTION CODE
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)    {
+        data = item;
+        left = right = null;
+    }
+} */
+
+/* Should return minimum distance between a and b
+   in a tree with given root*/
+class GfG {
+    int findDist(Node root, int a, int b) {
+       Node common =  findCommonAncestor(root,a,b);
+       return findDeptOfNode(common,a,0)+findDeptOfNode(common,b,0);
+    }
+
+    private int findDeptOfNode(Node root, int Value,int depth) {
+       if(root==null) return 0;
+       if(root.data== Value) return depth;
+
+       return Math.max(findDeptOfNode(root.left,Value,depth+1), findDeptOfNode(root.right,Value,depth+1));
+    }
+
+    private Node findCommonAncestor(Node root, int a, int b) {
+        if(root==null) return null;
+        else if(findNodeInTree(root.left,a,b)) return findCommonAncestor(root.left,a,b);
+        else if (findNodeInTree(root.right,a,b)) return findCommonAncestor(root.right,a,b);
+        return root;
+    }
+
+    private boolean findNodeInTree(Node node, int a, int b) {
+
+       boolean[] foundAB = new boolean[2];
+       helpermee(foundAB,a,b,node);
+       return foundAB[0] && foundAB[1];
+    }
+
+    private void helpermee(boolean[] foundAB, int a, int b,Node node) {
+        if(node==null) return;
+        if(a==node.data) foundAB[0] = true;
+        if(b==node.data) foundAB[1] = true;
+        if (foundAB[0] && foundAB[1])  return;
+        helpermee(foundAB,a,b,node.left);
+        helpermee(foundAB,a,b,node.right);
+    }
+}
