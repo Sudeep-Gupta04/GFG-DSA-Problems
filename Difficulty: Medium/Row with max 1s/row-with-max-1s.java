da6cd6558cc24c2b96cec74229768b56496e3f65
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2025-03-25 02:37:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt(); // Number of test cases
+        sc.nextLine();        // Consume the newline character
+
+        while (t-- > 0) {
+            String input = sc.nextLine();
+
+            // Replace ][ with ],[
+            input = input.replace("][", "],[");
+
+            ArrayList<ArrayList<Integer>> mat = new ArrayList<>();
+            String[] rows = input.split("],\\s*\\[");
+
+            for (String row : rows) {
+                row = row.replaceAll("[\\[\\]]", ""); // Remove any surrounding brackets
+                ArrayList<Integer> intRow = new ArrayList<>();
+                for (String num : row.split(",")) {
+                    intRow.add(Integer.parseInt(num.trim()));
+                }
+                mat.add(intRow);
+            }
+
+            Solution obj = new Solution();
+            int result = obj.rowWithMax1s(convertListToArray(mat));
+
+            System.out.println(result);
+
+            System.out.println("~");
+        }
+        sc.close();
+    }
+
+    // Helper method to convert ArrayList<ArrayList<Integer>> to 2D array
+    public static int[][] convertListToArray(ArrayList<ArrayList<Integer>> mat) {
+        int rows = mat.size();
+        int cols = mat.get(0).size();
+        int[][] arr = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = mat.get(i).get(j);
+            }
+        }
+        return arr;
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int rowWithMax1s(int arr[][]) {
+        int idx = -1;
+        int onss = 0;
+        // code here
+        for(int i =0;i<arr.length;i++){
+            int s = 0; int e= arr[0].length-1;
+            
+            while(s<=e){
+                int mid = (s+e)/2;
+                if(arr[i][mid]==1){
+                    e = mid-1;
+                }else{
+                    s = mid +1;
+                }
+            }
+            if(arr[0].length-s>onss){
+                onss = arr[0].length-s;
+                idx = i;
+            }
+        }
+        return idx;
+    }
+}
