4dc06921acfa73067146b5d5d21db2b65c18ed73
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2025-03-28 03:24:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove duplicates from an unsorted linked list/remove-duplicates-from-an-unsorted-linked-list.java
+++ Difficulty: Easy/Remove duplicates from an unsorted linked list/remove-duplicates-from-an-unsorted-linked-list.java
@@ -1,83 +1,9 @@
 //{ Driver Code Starts
-/* package whatever; // don't place package name! */
-import java.util.*;
 import java.io.*;
-class Node
-{
-    int data;
-    Node next;
-    Node(int d) {
-        data = d; 
-        next = null;
-    }
-}
+import java.util.HashSet;
 
-class Remove_Duplicate_From_LL
-{
-    Node head;  
-    Node temp;
-	public void addToTheLast(Node node) 
-	{
-
-	  if (head == null) 
-	  {
-	    head = node;
-	    temp = node;
-	  }
-	  else{
-	      temp.next = node;
-	      temp = node;
-	  }
-	}
-
-      void printList(PrintWriter out)
-    {
-        Node temp = head;
-        while (temp != null)
-        {
-           out.print(temp.data+" ");
-           temp = temp.next;
-        }  
-        out.println();
-    }
-	
-	/* Drier program to test above functions */
-	public static void main(String args[])throws IOException
-    {
-       
-        
-        /* Constructed Linked List is 1->2->3->4->5->6->
-           7->8->8->9->null */
-         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
-         PrintWriter out=new PrintWriter(System.out);
-		 int t=Integer.parseInt(in.readLine().trim());
-		  
-		 while(t>0)
-         {
-			int n = Integer.parseInt(in.readLine().trim());
-			Remove_Duplicate_From_LL llist = new Remove_Duplicate_From_LL();
-			String s[]=in.readLine().trim().split(" ");
-			int a1=Integer.parseInt(s[0]);
-			Node head= new Node(a1);
-            llist.addToTheLast(head);
-            for (int i = 1; i < n; i++) 
-			{
-				int a = Integer.parseInt(s[i]); 
-				llist.addToTheLast(new Node(a));
-			}
-		//llist.printList();	
-        Solution g = new Solution();
-		llist.head = g.removeDuplicates(llist.head);
-		llist.printList(out);
-		
-        t--;		
-        }
-        out.close();
-    }
-}
 
 // } Driver Code Ends
-
 
 /* The structure of linked list is the following
 class Node
@@ -85,28 +11,87 @@ class Node
     int data;
     Node next;
     Node(int d) {
-        data = d; 
+        data = d;
         next = null;
     }
 }
 */
 
-class Solution
-{
-    //Function to remove duplicates from unsorted linked list.
-    public Node removeDuplicates(Node head)
-    {
-        Node temp = head;
+class Solution {
+    // Function to remove duplicates from unsorted linked list.
+    public Node removeDuplicates(Node head) {
+        if(head==null || head.next==null) return head;
         HashSet<Integer> hs = new HashSet<>();
-        while (temp.next!=null && temp!=null){
-            if(temp.data==temp.next.data || hs.contains(temp.next.data)) {
-                temp.next = temp.next.next;
-            } else if (temp.data!=temp.next.data) {
+        hs.add(head.data);
+        Node temp = head.next;
+        Node h = head;
+        while(temp!=null){
+            if(!hs.contains(temp.data)){
                 hs.add(temp.data);
+                h = temp;
                 temp = temp.next;
+            }else{
+                h.next = temp.next;
+                temp = h.next;
             }
-
         }
         return head;
     }
 }
+
+
+
+//{ Driver Code Starts.
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Driver_code {
+    static Node insert(Node head, int data) {
+        Node temp = new Node(data);
+        if (head == null) {
+            return temp;
+        } else {
+            Node t = head;
+            while (t.next != null) {
+                t = t.next;
+            }
+            t.next = temp;
+        }
+        return head;
+    }
+
+    static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            Node head = null;
+            String[] str = read.readLine().trim().split(" ");
+            for (String s : str) {
+                head = insert(head, Integer.parseInt(s));
+            }
+            Solution g = new Solution();
+            head = g.removeDuplicates(head);
+            printList(head);
+        }
+    }
+}
+
+// } Driver Code Ends
