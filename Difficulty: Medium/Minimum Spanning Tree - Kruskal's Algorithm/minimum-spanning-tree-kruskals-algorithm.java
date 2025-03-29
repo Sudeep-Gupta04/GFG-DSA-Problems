4e74e76e9c4b5debd5b214cb1ee7757d0eebc866
Sudeep Gupta <sudeepkgupta2001@gmail.com>
2025-03-30 01:20:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Spanning Tree - Kruskal's Algorithm/minimum-spanning-tree-kruskals-algorithm.java
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br;
+    static PrintWriter ot;
+
+    public static void main(String args[]) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int V = Integer.parseInt(br.readLine().trim());
+            int E = Integer.parseInt(br.readLine().trim());
+            int[][] edges = new int[E][3];
+            for (int i = 0; i < E; i++) {
+                String[] s = br.readLine().trim().split(" ");
+                edges[i][0] = Integer.parseInt(s[0]);
+                edges[i][1] = Integer.parseInt(s[1]);
+                edges[i][2] = Integer.parseInt(s[2]);
+            }
+            ot.println(new Solution().kruskalsMST(V, edges));
+            ot.println("~");
+        }
+        ot.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static int kruskalsMST(int V, int[][] edges) {
+        // code here
+        Arrays.sort(edges, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[2]-o2[2];
+            }
+        });
+        DSU dsu = new DSU(V);
+        int ans = 0;
+        for(int i =0;i< edges.length;i++){
+            int x = edges[i][0];
+            int y = edges[i][1];
+            if(dsu.find(x)!=dsu.find(y)){
+                ans+=edges[i][2];
+                dsu.union(x,y);
+            }
+        }
+        return ans;
+    }
+}
+class DSU {
+    private int[] parent, rank;
+
+    public DSU(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    public int find(int i) {
+        if (parent[i] != i) {
+            parent[i] = find(parent[i]);
+        }
+        return parent[i];
+    }
+
+    public void union(int x, int y) {
+        int s1 = find(x);
+        int s2 = find(y);
+        if (s1 != s2) {
+            if (rank[s1] < rank[s2]) {
+                parent[s1] = s2;
+            } else if (rank[s1] > rank[s2]) {
+                parent[s2] = s1;
+            } else {
+                parent[s2] = s1;
+                rank[s1]++;
+            }
+        }
+    }
+}
